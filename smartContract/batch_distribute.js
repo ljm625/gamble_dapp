02d4a3418ad8d427d6166c9f1af0f06a971597cc
Jiaming Li <ljm625@gmail.com>
2018-05-15 17:32:37 +0800

    fix issues

--- smartContract/batch_distribute.js
+++ smartContract/batch_distribute.js
@@ -1,128 +1,30 @@
 "use strict";
 
-// Batch distribute NAS to multiple accounts.
-
-var DictItem = function(text) {
-	if (text) {
-		var obj = JSON.parse(text);
-		this.key = obj.key;
-		this.value = obj.value;
-		this.author = obj.author;
-	} else {
-	    this.key = "";
-	    this.author = "";
-	    this.value = "";
-	}
-};
-
-DictItem.prototype = {
-	toString: function () {
-		return JSON.stringify(this);
-	}
-};
-
 var BatchDistribute = function () {
-    LocalContractStorage.defineMapProperty(this, "repo", {
-        parse: function (text) {
-            return new DictItem(text);
-        },
-        stringify: function (o) {
-            return o.toString();
-        }
+    LocalContractStorage.defineProperties(this, {
+        balance: null,
+        owner: null
     });
+    LocalContractStorage.defineMapProperty(this, "balancemap");
 };
 
 BatchDistribute.prototype = {
     init: function () {
-        // todo
-        // Set the developer address
-        this.repo.put("dev","n1Y9JLyHUdHqFTvZD9S2a5acx6vsPeTHFCJ");
-
+        console.log("test");
     },
 
     distribute_equal: function (address_list) {
         // deposit money into multiple address.
         // If, for some reason, some deposit failed (For example bad address), we should still allow the user to retain the balance from his account.
         // Need to check if throw function will rollback transfer action.
-
+        
         var number = address_list.length;
-        var each_amount = Blockchain.transaction.value.idiv(number);
+        var total_amount = new BigNumber(Blockchain.transaction.value);
+        var each_amount = total_amount.idiv(number);
         for (var address in address_list){
-            Blockchain.transfer(address,each_amount)
+            Blockchain.transfer(address,each_amount);
         }
 
     }
-
-
-
-    //     var par_list = this.repo.get("participant_list");
-    //     var current_num = new BigNumber(this.repo.get("cur_number"));
-    //     var target = new BigNumber(this.repo.get("target"));
-    //     if (current_num.plus(Blockchain.transaction.value).plus(1) > target){
-    //         // The Game is ended. Now start lucky chooser
-    //         par_list[Blockchain.transaction.from]={start:current_num.plus(1).toString(), end: target.toString()};
-    //         // refund the extra part first.
-    //         var extra = target.minus(current_num);
-    //         var refund = Blockchain.transaction.value.minus(extra);
-    //         current_num = target;
-    //         Blockchain.transfer(Blockchain.transaction.from,refund);
-
-    //         var winner = Math.floor(Math.random() * (target+1));
-    //         var winner_address=undefined;
-    //         // Find the winner now
-    //         for (var key in par_list) {
-    //             if(par_list.hasOwnProperty(key)){
-    //                 if(par_list.end <= winner && par_list.start >= winner){
-    //                     // This is the winner
-    //                     winner_address = key;
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //         if(winner_address!== undefined){
-    //             Blockchain.transfer()
-    //         }
-            
-
-
-    //     }
-    //     else{
-    //         par_list[Blockchain.transaction.from]={start:current_num.plus(1).toString(), end: current_num.plus(Blockchain.transaction.value).plus(1).toString()};
-    //         current_num = current_num.plus(1).plus(Blockchain.transaction.value);
-    //         this.repo.put("cur_number", current_num.toString());
-    
-    //     }
-
-    //     // Lucky number system : address, start_num, end_num
-    //     key = key.trim();
-    //     value = value.trim();
-    //     if (key === "" || value === ""){
-    //         throw new Error("empty key / value");
-    //     }
-    //     if (value.length > 64 || key.length > 64){
-    //         throw new Error("key / value exceed limit length")
-    //     }
-
-    //     var from = Blockchain.transaction.from;
-    //     var dictItem = this.repo.get(key);
-    //     if (dictItem){
-    //         throw new Error("value has been occupied");
-    //     }
-
-    //     dictItem = new DictItem();
-    //     dictItem.author = from;
-    //     dictItem.key = key;
-    //     dictItem.value = value;
-
-    //     this.repo.put(key, dictItem);
-    // },
-
-    // get: function (key) {
-    //     key = key.trim();
-    //     if ( key === "" ) {
-    //         throw new Error("empty key")
-    //     }
-    //     return this.repo.get(key);
-    // }
 };
 module.exports = BatchDistribute;
